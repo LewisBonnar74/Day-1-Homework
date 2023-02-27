be5d2701ec8bd846ef06c2f9fd5f00918007a05b
Lewis Bonnar <lewisbonnar74@gmail.com>
2023-02-27 19:22:05 +0000

    second commit

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,15 +1,9 @@
-current_week = 1
-current_day_of_week = 1
-total_course_weeks = 16
-total_course_days_per_week = 5
+stock_sold_today = 20
+total_store_stock = 100
 
-def weeks_to_go():
-    weeks_left = total_course_weeks - current_week
-    days_left = total_course_days_per_week - current_day_of_week
-    print(f"Only {weeks_left} weeks and {days_left} days to go!")
+def stock_check():
+    remaining_stock = total_store_stock - stock_sold_today
+    print(f"{remaining_stock} units of stock remain")
 
-def motivate_me():
-    print("We got this!! You're doing great!!!")
 
-weeks_to_go()
-motivate_me()
+stock_check()
